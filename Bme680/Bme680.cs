cbacf98e2e73ea3de7a3306935bbd485d6264da6
George Mathieson <george@mathieson.me>
2019-05-22 21:42:03 +0100

    Add the Bme680 class

---
+++ Bme680/Bme680.cs
@@ -0,0 +1,24 @@
+﻿using System.Device.I2c;
+
+namespace Bme680
+{
+    /// <summary>
+    /// Represents a BME680 gas, temperature, humidity and pressure sensor over the I2C serial protocol.
+    /// </summary>
+    public class Bme680
+    {
+        /// <summary>
+        /// Pipeline to the <see cref="I2cDevice"/>.
+        /// </summary>
+        internal I2cDevice _i2cDevice;
+
+        /// <summary>
+        /// Initialise a new instance of the <see cref="Bme680"/> class.
+        /// </summary>
+        /// <param name="i2cDevice">The <see cref="I2cDevice"/> to create with.</param>
+        public Bme680(I2cDevice i2cDevice)
+        {
+            _i2cDevice = i2cDevice;
+        }
+    }
+}
