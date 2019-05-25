edbaa5ff0a4d66347a852a41c0bb278b267f1f2f
George Mathieson <george@mathieson.me>
2019-05-25 14:09:11 +0100

    Add oversampling

---
+++ Bme680/Oversampling.cs
@@ -0,0 +1,38 @@
+﻿namespace Bme680
+{
+    /// <summary>
+    /// Oversampling settings.
+    /// </summary>
+    public enum Oversampling : byte
+    {
+        /// <summary>
+        /// Skipped (output set to 0x8000).
+        /// </summary>
+        Skipped = 0b000,
+
+        /// <summary>
+        /// Oversampling x 1.
+        /// </summary>
+        x1 = 0b001,
+
+        /// <summary>
+        /// Oversampling x 2.
+        /// </summary>
+        x2 = 0b010,
+
+        /// <summary>
+        /// Oversampling x 4.
+        /// </summary>
+        x4 = 0b011,
+
+        /// <summary>
+        /// Oversampling x 8.
+        /// </summary>
+        x8 = 0b100,
+
+        /// <summary>
+        /// Oversampling x 16.
+        /// </summary>
+        x16 = 0b101
+    }
+}
