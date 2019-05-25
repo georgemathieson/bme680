1e71b47c2c374d850735101659cbdcd2cedbf5a6
George Mathieson <george@mathieson.me>
2019-05-25 15:54:32 +0100

    Add general control registers with chip id

---
+++ Bme680.Tests/RegisterTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Bme680.Tests
+{
+    /// <summary>
+    /// Unit tests for <see cref="Register"/>.
+    /// </summary>
+    public class RegisterTests
+    {
+        /// <summary>
+        /// Ensure that <see cref="Register.ChipId"/> is set to 0xD0.
+        /// </summary>
+        /// <remarks>
+        /// 0xD0 in I2C.
+        /// </remarks>
+        [Fact]
+        public void ChipId_HasValue_0xD0()
+        {
+            // Arrange.
+            byte expected = 0xD0;
+
+            // Act.
+            var actual = (byte)Register.ChipId;
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Bme680/Register.cs
@@ -0,0 +1,13 @@
+﻿namespace Bme680
+{
+    /// <summary>
+    /// General control registers.
+    /// </summary>
+    public enum Register : byte
+    {
+        /// <summary>
+        /// Chip ID of the device.
+        /// </summary>
+        ChipId = 0xD0
+    }
+}
