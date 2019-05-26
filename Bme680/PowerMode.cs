79861d72e2188d3c4d09ed8422cacfbe1d2945d2
George Mathieson <george@mathieson.me>
2019-05-26 17:35:14 +0100

    Add power mode settings

---
+++ Bme680/PowerMode.cs
@@ -0,0 +1,28 @@
+﻿namespace Bme680
+{
+    /// <summary>
+    /// Sensor power mode.
+    /// </summary>
+    /// <remarks>
+    /// Section 3.1 in the datasheet.
+    /// </remarks>
+    public enum PowerMode : byte
+    {
+        /// <summary>
+        /// No measurements are performed.
+        /// </summary>
+        /// <remarks>
+        /// Minimal power consumption.
+        /// </remarks>
+        Sleep = 0b00,
+
+        /// <summary>
+        /// Single TPHG cycle is performed.
+        /// </summary>
+        /// <remarks>
+        /// Sensor automatically returns to sleep mode afterwards.
+        /// Gas sensor heater only operates during gas measurement.
+        /// </remarks>
+        Forced = 0b01
+    }
+}
