7befdff9f9cbd5a90b2eca02454e10160dd71bb5
George Mathieson <george@mathieson.me>
2019-05-26 16:48:32 +0100

    Update the Bme680 class to use the communications wrapper

--- Bme680/Bme680.cs
+++ Bme680/Bme680.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Device.I2c;
-using Iot.Units;
+using Bme680.Com;
 
 namespace Bme680
 {
@@ -10,12 +9,12 @@ namespace Bme680
     public sealed class Bme680 : IDisposable
     {
         /// <summary>
-        /// Default I2C address.
+        /// Default I2C bus address.
         /// </summary>
         public const byte DefaultI2cAddress = 0x76;
 
         /// <summary>
-        /// Secondary I2C address.
+        /// Secondary I2C bus address.
         /// </summary>
         public const byte SecondaryI2cAddress = 0x77;
 
@@ -25,32 +24,32 @@ namespace Bme680
         private const byte _expectedChipId = 0x61;
 
         /// <summary>
-        /// The communications channel to a device on an I2C bus.
+        /// The communications channel to a device on a communications bus.
         /// </summary>
-        private I2cDevice _i2cDevice;
+        private IComDevice _comDevice;
 
         /// <summary>
         /// Initialize a new instance of the <see cref="Bme680"/> class.
         /// </summary>
-        /// <param name="i2cDevice">The <see cref="I2cDevice"/> to create with.</param>
+        /// <param name="comDevice">The <see cref="IComDevice"/> to create with.</param>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
         /// <exception cref="Bme680Exception"></exception>
-        public Bme680(I2cDevice i2cDevice)
+        public Bme680(IComDevice comDevice)
         {
-            _i2cDevice = i2cDevice ?? throw new ArgumentNullException(nameof(i2cDevice));
+            _comDevice = comDevice ?? throw new ArgumentNullException(nameof(comDevice));
 
             // Ensure a valid device address has been set.
-            var deviceAddress = i2cDevice.ConnectionSettings.DeviceAddress;
+            var deviceAddress = comDevice.DeviceAddress;
             if (deviceAddress < DefaultI2cAddress || deviceAddress > SecondaryI2cAddress)
             {
-                throw new ArgumentOutOfRangeException(nameof(i2cDevice),
+                throw new ArgumentOutOfRangeException(nameof(comDevice),
                     $"Chip address 0x{deviceAddress.ToString("X2")} is out of range for a BME680. Expected 0x{DefaultI2cAddress.ToString("X2")} or 0x{SecondaryI2cAddress.ToString("X2")}");
             }
 
-            // Ensure the device exists on the bus.
-            _i2cDevice.WriteByte((byte)Register.ChipId);
-            var readChipId = _i2cDevice.ReadByte();
+            // Ensure the device exists on the I2C bus.
+            _comDevice.WriteByte((byte)Register.Id);
+            var readChipId = _comDevice.ReadByte();
             if (readChipId != _expectedChipId)
             {
                 throw new Bme680Exception(
@@ -63,8 +62,8 @@ namespace Bme680
         /// </summary>
         public void Dispose()
         {
-            _i2cDevice?.Dispose();
-            _i2cDevice = null;
+            _comDevice?.Dispose();
+            _comDevice = null;
         }
     }
 }
