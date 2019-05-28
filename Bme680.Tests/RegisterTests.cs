8973b1287918eec805663d1278d10d512a9484a6
George Mathieson <george@mathieson.me>
2019-05-28 22:07:45 +0100

    Add test for measure status register and order tests alphabetically

--- Bme680.Tests/RegisterTests.cs
+++ Bme680.Tests/RegisterTests.cs
@@ -8,19 +8,16 @@ namespace Bme680.Tests
     public class RegisterTests
     {
         /// <summary>
-        /// Ensure that <see cref="Register.Id"/> is set to 0xD0.
+        /// Ensure that <see cref="Register.eas_status_0"/> is set to 0x1D
         /// </summary>
-        /// <remarks>
-        /// 0xD0 for I2C.
-        /// </remarks>
         [Fact]
-        public void Id_HasValue_0xD0()
+        public void eas_status_0_HasValue_0x1D()
         {
             // Arrange.
-            byte expected = 0xD0;
+            byte expected = 0x1D;
 
             // Act.
-            var actual = (byte)Register.Id;
+            var actual = (byte)Register.eas_status_0;
 
             // Assert.
             Assert.Equal(expected, actual);
@@ -43,48 +40,19 @@ namespace Bme680.Tests
         }
 
         /// <summary>
-        /// Ensure that <see cref="Register.temp_msb"/> is set to 0x22.
+        /// Ensure that <see cref="Register.Id"/> is set to 0xD0.
         /// </summary>
+        /// <remarks>
+        /// 0xD0 for I2C.
+        /// </remarks>
         [Fact]
-        public void temp_msb_HasValue_0x22()
+        public void Id_HasValue_0xD0()
         {
             // Arrange.
-            byte expected = 0x22;
+            byte expected = 0xD0;
 
             // Act.
-            var actual = (byte)Register.temp_msb;
-
-            // Assert.
-            Assert.Equal(expected, actual);
-        }
-
-        /// <summary>
-        /// Ensure that <see cref="Register.temp_lsb"/> is set to 0x23.
-        /// </summary>
-        [Fact]
-        public void temp_lsb_HasValue_0x23()
-        {
-            // Arrange.
-            byte expected = 0x23;
-
-            // Act.
-            var actual = (byte)Register.temp_lsb;
-
-            // Assert.
-            Assert.Equal(expected, actual);
-        }
-
-        /// <summary>
-        /// Ensure that <see cref="Register.temp_xlsb"/> is set to 0x24.
-        /// </summary>
-        [Fact]
-        public void temp_lsb_HasValue_0x24()
-        {
-            // Arrange.
-            byte expected = 0x24;
-
-            // Act.
-            var actual = (byte)Register.temp_xlsb;
+            var actual = (byte)Register.Id;
 
             // Assert.
             Assert.Equal(expected, actual);
@@ -133,6 +101,54 @@ namespace Bme680.Tests
 
             // Act.
             var actual = (byte)Register.temp_cal_3;
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Ensure that <see cref="Register.temp_lsb"/> is set to 0x23.
+        /// </summary>
+        [Fact]
+        public void temp_lsb_HasValue_0x23()
+        {
+            // Arrange.
+            byte expected = 0x23;
+
+            // Act.
+            var actual = (byte)Register.temp_lsb;
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Ensure that <see cref="Register.temp_msb"/> is set to 0x22.
+        /// </summary>
+        [Fact]
+        public void temp_msb_HasValue_0x22()
+        {
+            // Arrange.
+            byte expected = 0x22;
+
+            // Act.
+            var actual = (byte)Register.temp_msb;
+
+            // Assert.
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// Ensure that <see cref="Register.temp_xlsb"/> is set to 0x24.
+        /// </summary>
+        [Fact]
+        public void temp_xlsb_HasValue_0x24()
+        {
+            // Arrange.
+            byte expected = 0x24;
+
+            // Act.
+            var actual = (byte)Register.temp_xlsb;
 
             // Assert.
             Assert.Equal(expected, actual);
