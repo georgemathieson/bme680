cccbe52dbc7ed437529629a8c0db863130bd7cf6
George Mathieson <george@mathieson.me>
2019-05-28 22:19:49 +0100

    Add a way to get the value of the new data bit

--- Bme680/Bme680.cs
+++ Bme680/Bme680.cs
@@ -67,6 +67,17 @@ namespace Bme680
         }
 
         /// <summary>
+        /// Gets a value indicating whether or not new sensor data is available.
+        /// </summary>
+        /// <returns>True if new data is available.</returns>
+        public bool HasNewData()
+        {
+            byte status = Read8Bits(Register.eas_status_0);
+
+            return (status >> 7) != 0;
+        }
+
+        /// <summary>
         /// Set the power mode.
         /// </summary>
         /// <param name="powerMode">The <see cref="PowerMode"/> to set.</param>
