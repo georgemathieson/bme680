ce5a60441d6bbf68e32eb817bb1b55281c6768ed
George Mathieson <george@mathieson.me>
2019-05-30 21:53:51 +0100

    Update the sample file to only get new data

--- Bme680.Samples/Program.cs
+++ Bme680.Samples/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Device.I2c;
 using System.Device.I2c.Drivers;
+using System.Threading;
 
 namespace Bme680.Samples
 {
@@ -25,10 +26,26 @@ namespace Bme680.Samples
             using (var bme680 = new Bme680(unixI2cDevice))
             {
                 bme680.SetTemperatureOversampling(Oversampling.x2);
-                bme680.SetPowerMode(PowerMode.Forced);
-                var temperature = bme680.ReadTemperature();
 
-                Console.WriteLine($"Celsius: {temperature.Celsius}");
+                while (true)
+                {
+                    // Once a reading has been taken, the sensor goes back to sleep mode.
+                    if (bme680.GetPowerMode() == PowerMode.Sleep)
+                    {
+                        // This instructs the sensor to take a measurement.
+                        bme680.SetPowerMode(PowerMode.Forced);
+                    }
+
+                    // This prevent us from reading old data from the sensor.
+                    if (bme680.HasNewData())
+                    {
+                        var temperature = bme680.ReadTemperature();
+
+                        Console.WriteLine($"{Math.Round(temperature.Celsius, 2).ToString("N2")}°c");
+
+                        Thread.Sleep(100);
+                    }
+                }
             }
         }
     }
